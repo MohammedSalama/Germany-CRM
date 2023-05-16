ad4017e0c3a45ee06b19f31d82ea769efd7ee1b7
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-05-16 11:43:02 +0300

    InvoiceDatabase

--- database/migrations/2023_05_13_133212_create_invoices_table.php
+++ database/migrations/2023_05_13_133212_create_invoices_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->float('total');
+            $table->text('items');
+            $table->integer('customer_id');
+            $table->boolean('status')->default(0);
             $table->timestamps();
         });
     }
