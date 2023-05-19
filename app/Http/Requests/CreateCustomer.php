75024c7efed795915d12325c0778dd00c6ddefe5
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-05-20 01:47:34 +0300

    ServiceContainer

--- app/Http/Requests/CreateCustomer.php
+++ app/Http/Requests/CreateCustomer.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Crm\Base\Requests\ApiRequest;
 
 class CreateCustomer extends ApiRequest
 {
