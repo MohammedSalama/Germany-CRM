abc59f6de671e8b42cbf9ad7d004bd5f9e148205
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-05-24 03:17:08 +0300

    Breeze_Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/adminDashboard', function () {
-    return view('Backend.adminDashboard');
-})->name('adminDashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+require __DIR__.'/auth.php';
