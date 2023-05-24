97edd3512a936078e73ed4062b709933c8497723
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-05-24 15:27:23 +0300

    Seeder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,12 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/adminDashboard', function () {
+    return view('Backend.adminDashboard');
+})->middleware(['auth', 'verified'])->name('adminDashboard');
 
 
 require __DIR__.'/auth.php';
